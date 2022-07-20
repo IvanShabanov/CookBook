c3e846c0c7294c42d9360d5bdc86a6e9b9744263
Иван Шабанов <coder10@web-axioma.ru>
2022-07-20 11:00:11 +0700

    feat: bitrix элементы и свойтва

---
+++ php/Bitrix/Bitrix получение элементов и их свойтв из инфоблока.php
@@ -0,0 +1,32 @@
+<?
+    /* выберим нужные элементы инфоблока */
+    $rsElements = \Bitrix\Iblock\ElementTable::getList([
+		'filter' => [
+			'ID' => $arID,
+			'ACTIVE' => 'Y'
+		],
+		'select' => [
+			'ID',
+			'IBLOCK_ID',
+			'*'
+		]
+	]);
+
+	while ($arEl = $rsElements->fetch()) {
+        /* Получим свойтва элемента */
+		$obProp = \CIBlockElement::GetProperty(
+			$arEl['IBLOCK_ID'],
+			$arEl['ID'],
+			[],
+			['CODE ' => [
+                /* Коды нужных свойств */
+				'PRICE',
+				'PRICE_PER',
+			]]
+		);
+		while ($arProp = $obProp->GetNext()) {
+			$arEl['PROPERTIES'][$arProp['CODE']] = $arProp['VALUE'];
+		};
+
+        print_r ($arEl);
+    };
