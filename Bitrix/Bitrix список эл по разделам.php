e8c020329fae80fdf45fe5107a32b3852c2a55de
Иван Шабанов <coder10@web-axioma.ru>
2022-12-16 15:35:12 +0700

    feat: вариант где елемент в нескольких разделах

--- Bitrix/Bitrix список эл по разделам.php
+++ Bitrix/Bitrix список эл по разделам.php
@@ -2,6 +2,42 @@
 /* result_modifier.php в шаблоне компонента news.list */
 if( !defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true ) die();
 
+foreach ($arResult['ITEMS'] as $arItem) {
+	/* Получим все разделы элемента */
+	$resSections = CIBlockElement::GetElementGroups($arItem['ID'], true);
+	$arSection = [];
+	while($arSection = $resSections->Fetch()) {
+		/* В массив разделов добавим элемент и сам раздел */
+		$arResult['SECTIONS'][$arSection['ID']]["ITEMS"][] = $arItem;
+		$arResult['SECTIONS'][$arSection['ID']]["SECTION"] = $arSection;
+	}
+}
+
+/* сортируем разделы по полю сортировка */
+if (count($arResult['SECTIONS']) > 1) {
+	$sortSections = [];
+	foreach ($arResult['SECTIONS'] as $arSection) {
+		$key = $arSection['SECTION']['SORT'];
+		while (isset($sortSections[$key])) {
+			$key++;
+		}
+		$sortSections[$key] = $arSection;
+	}
+	ksort($sortSections);
+	$arResult['SECTIONS'] = $sortSections;
+}
+
+
+
+
+
+
+
+/********************************************************************** */
+/* еще вариант, работает если элемент привязан только к одному разделу  */
+/********************************************************************** */
+if( !defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true ) die();
+
 
 foreach ($arResult['ITEMS'] as $arItem) {
 	/* В массив разделов добавим элемент */
