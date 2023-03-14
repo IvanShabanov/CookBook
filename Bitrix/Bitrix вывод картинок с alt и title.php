7d24b0a7e61955639445754675872a89b9522096
Ivan Shabanov <Ivan.shabanov@gmail.com>
2023-03-14 11:48:23 +0700

    feat: Bitrix вывод картинок с alt и title

---
+++ Bitrix/Bitrix вывод картинок с alt и title.php
@@ -0,0 +1,6 @@
+
+
+<img src="<?= $arItem['PREVIEW_PICTURE']['SRC']; ?>" alt="<?=(($arItem['PREVIEW_PICTURE']['ALT']) != '' ? $arItem['PREVIEW_PICTURE']['ALT'] : $arItem['NAME'])?>" title="<?=(($arItem['PREVIEW_PICTURE']['TITLE'] != '') ? $arItem['PREVIEW_PICTURE']['TITLE'] : $arItem['NAME'])?>" />
+
+
+<img src="<?= $arItem['DETAIL_PICTURE']['SRC']; ?>" alt="<?=(($arItem['DETAIL_PICTURE']['ALT']) != '' ? $arItem['PREVIEW_PICTURE']['ALT'] : $arItem['NAME'])?>" title="<?=(($arItem['DETAIL_PICTURE']['TITLE'] != '') ? $arItem['DETAIL_PICTURE']['TITLE'] : $arItem['NAME'])?>" />
