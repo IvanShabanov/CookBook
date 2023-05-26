d419b2d553513f668a3a94f6e2f8b5a3aa0f8b6a
Ivan Shabanov <Ivan.shabanov@gmail.com>
2023-05-26 11:26:40 +0700

    feat(Bitrix): ajax header

---
+++ Bitrix/ajax header.php
@@ -0,0 +1,16 @@
+<?if(!defined('NO_KEEP_STATISTIC')){
+	define('NO_KEEP_STATISTIC', true);
+}
+
+if(!defined('STOP_STATISTICS')){
+	define('STOP_STATISTICS', true);
+}
+
+if(!defined('NO_AGENT_STATISTIC')){
+	define('NO_AGENT_STATISTIC', true);
+}
+
+if(!defined('NO_AGENT_CHECK')){
+	define('NO_AGENT_CHECK', true);
+};
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");?>
