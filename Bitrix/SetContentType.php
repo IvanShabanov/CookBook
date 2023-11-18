a2176a45a50b61f906a68f16182549a2d4a2698b
Ivan Shabanov <Ivan.shabanov@gmail.com>
2023-11-18 13:47:18 +0700

    feat(Bitrix): set Content-Type

---
+++ Bitrix/SetContentType.php
@@ -0,0 +1,29 @@
+<?php
+set_time_limit(0);
+error_reporting(E_ERROR);
+
+// statistics off
+@define('STOP_STATISTIC', true);
+@define('NO_KEEP_STATISTIC', true);
+@define('NO_AGENT_STATISTIC', 'Y');
+
+// perfmon stop
+@define('PERFMON_STOP', true);
+
+// check permissions off
+@define('NOT_CHECK_PERMISSIONS', true);
+
+// dont show errors
+@define('PUBLIC_AJAX_MODE', true);
+
+// dont run agents
+@define('NO_AGENT_CHECK', true);
+
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php');
+global $APPLICATION, $DB, $USER;
+
+$APPLICATION->RestartBuffer();
+$app = \Bitrix\Main\Application::getInstance();
+$response = $app->getContext()->getResponse();
+$responseHeadersHandler = $response->getHeaders();
+$responseHeadersHandler->set('Content-Type', 'text/javascript; charset=UTF-8');
