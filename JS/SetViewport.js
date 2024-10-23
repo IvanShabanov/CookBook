7eda6356aac3416a28e4273dcdf8a46d59b05562
Ivan Shabanov <Ivan.shabanov@gmail.com>
2024-10-23 17:58:32 +0700

    feat(js/viewport): ES6

--- JS/SetViewport.js
+++ JS/SetViewport.js
@@ -1,18 +1,58 @@
 
-function InitViewport(minwidth) {
-    SetViewport(minwidth);
+function Viewport(setting) {
+    SetViewport(setting);
     window.addEventListener('resize', () => {
-        SetViewport(minwidth);
+        SetViewport(setting);
     })
-}
 
-function SetViewport(minwidth) {
-    const meta = document.querySelector('meta[name="viewport"]');
-    if (screen.width < minwidth) {
-        meta.setAttribute('content', 'user-scalable=no,width=' + minwidth);
-    } else {
-        meta.setAttribute('content', 'width=device-width,initial-scale=1');
+    function SetViewport(setting) {
+        const meta = document.querySelector('meta[name="viewport"]');
+        let content = 'width=device-width,initial-scale=1';
+        Object.entries(setting).forEach(entry => {
+            const [width, value] = entry;
+            if (screen.width >= value[0] && screen.width < value[1]) {
+                content = 'user-scalable=no,width=' + width;
+            }
+        });
+        meta.setAttribute('content', content);
     }
 }
 
-InitViewport(360);
+Viewport(
+    {
+        /* viewport_width: [from_width, to_width] */
+        360: [0, 400],
+        578: [400, 578],
+    }
+);
+
+
+/* ES6 */
+class Viewport {
+    consructor(setting) {
+        const THIS = this;
+        THIS.SetViewport(setting);
+        window.addEventListener('resize', () => {
+            THIS.SetViewport(setting);
+        })
+    }
+
+    SetViewport(setting) {
+        const meta = document.querySelector('meta[name="viewport"]');
+        let content = 'width=device-width,initial-scale=1';
+        Object.entries(setting).forEach(entry => {
+            const [width, value] = entry;
+            if (screen.width >= value[0] && screen.width < value[1]) {
+                content = 'user-scalable=no,width=' + width;
+            }
+        });
+        meta.setAttribute('content', content);
+    }
+}
+new Viewport(
+    {
+        /* viewport_width: [from_width, to_width] */
+        360: [0, 400],
+        578: [400, 578],
+    }
+)
