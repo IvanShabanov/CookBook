aabededa3ec7827303094271072c0d9cd8cffe77
Ivan Shabanov <Ivan.shabanov@gmail.com>
2024-10-23 18:18:03 +0700

    feat(js/viewport): defContent

--- JS/SetViewport.js
+++ JS/SetViewport.js
@@ -1,13 +1,20 @@
-
+/* ES5 */
 function Viewport(setting) {
-    SetViewport(setting);
-    window.addEventListener('resize', () => {
-        SetViewport(setting);
-    })
+    const defContent = 'width=device-width,initial-scale=1';
+    const CreateMeta = () => {
+        let meta = document.querySelector('meta[name="viewport"]');
+        if (typeof meta != 'undefined') {
+            return;
+        }
+        meta = document.createElement('meta');
+        meta.name = 'viewport';
+        meta.content = defContent;
+        document.head.appendChild(meta);
+    }
 
-    function SetViewport(setting) {
+    const SetViewport = (setting) => {
         const meta = document.querySelector('meta[name="viewport"]');
-        let content = 'width=device-width,initial-scale=1';
+        let content = defContent;
         Object.entries(setting).forEach(entry => {
             const [width, value] = entry;
             if (screen.width >= value[0] && screen.width < value[1]) {
@@ -16,6 +23,12 @@ function Viewport(setting) {
         });
         meta.setAttribute('content', content);
     }
+
+    CreateMeta();
+    SetViewport(setting);
+    window.addEventListener('resize', () => {
+        SetViewport(setting);
+    })
 }
 
 Viewport(
@@ -30,16 +43,29 @@ Viewport(
 /* ES6 */
 class Viewport {
     consructor(setting) {
+        this.defContent = 'width=device-width,initial-scale=1';
         const THIS = this;
+        THIS.CreateMeta();
         THIS.SetViewport(setting);
         window.addEventListener('resize', () => {
             THIS.SetViewport(setting);
         })
     }
 
+    CreateMeta() {
+        let meta = document.querySelector('meta[name="viewport"]');
+        if (typeof meta != 'undefined') {
+            return;
+        }
+        meta = document.createElement('meta');
+        meta.name = 'viewport';
+        meta.content = this.defContent;
+        document.head.appendChild(meta);
+    }
+
     SetViewport(setting) {
         const meta = document.querySelector('meta[name="viewport"]');
-        let content = 'width=device-width,initial-scale=1';
+        let content = this.defContent;
         Object.entries(setting).forEach(entry => {
             const [width, value] = entry;
             if (screen.width >= value[0] && screen.width < value[1]) {
@@ -48,6 +74,7 @@ class Viewport {
         });
         meta.setAttribute('content', content);
     }
+
 }
 new Viewport(
     {
