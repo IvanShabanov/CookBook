e885ae038cc59dde4951e960961bb27fced71982
Ivan Shabanov <Ivan.shabanov@gmail.com>
2025-04-17 11:56:12 +0700

    feat(Print_r_tree): документирование и мелкие правки

--- Bitrix/print_r_tree.php
+++ Bitrix/print_r_tree.php
@@ -1,33 +1,42 @@
 <?
+/**
+ * print_r_tree - выведит в браузере расскрывающийся массив
+ * @param mixed $data - данные / массив для вывода
+ * @param mixed $level - уровень волженности (не нужно заполнять)
+ * @param mixed $parent - Наименование родителя (не нужно заполнять)
+ * @return string
+ */
 function print_r_tree($data, $level = 0, $parent = '')
 {
 	global $USER;
 	$out = '';
 	if ($level == 0) {
-		$out .= '<script  data-skip-moving="true">function showinner(el) {if(el.nextSibling.style.display == "block") {el.nextSibling.style.display = "none";} else {el.nextSibling.style.display = "block";};}; </script>';
+		$out .= '<script data-skip-moving="true">function showinner(el) {if(el.nextSibling.style.display == "block") {el.nextSibling.style.display = "none";} else {el.nextSibling.style.display = "block";};}; </script>';
 	}
 	$type = gettype($data);
 	if ($type == 'array') {
 		if ($level > 0) {
 			$out .= '<div style="display: none; padding: 5px; border: 1px solid #000;">';
-		};
+		}
 		foreach ($data as $key => $val) {
-			$item = '["' . $key . '"]';
+			$item    = '["' . $key . '"]';
 			$valtype = gettype($val);
 			if ($valtype == 'array') {
 				$count = '(' . count($val) . ')';
 			} else if (in_array($valtype, array('integer', 'double', 'float', 'string'))) {
 				$count = '(' . strlen($val) . ')';
-			};
+			} else {
+				$count = '';
+			}
 			$out .= '<div><div onclick="showinner(this);" style="padding: 5px;">' . gettype($val) . ' ' . $parent . $item . ' &nbsp; &nbsp; ' . $count . '</div>';
 
 			$out .= print_r_tree($val, $level + 1, $parent . $item);
 
 			$out .= '</div>';
-		};
+		}
 		if ($level > 0) {
 			$out .= '</div>';
-		};
+		}
 	} else if ($type == 'boolean') {
 		if ($data) {
 			$out .= '<pre style="display: none;  padding: 5px; border: 1px solid #000;">TRUE</pre>';
@@ -38,26 +47,33 @@ function print_r_tree($data, $level = 0, $parent = '')
 		$out .= '<pre style="display: none;  padding: 5px; border: 1px solid #000;">' . str_replace('<', '&lt;', $data) . '</pre>';
 	} else {
 		$out .= '<pre style="display: none;  padding: 5px; border: 1px solid #000;">' . print_r($data, true) . '</pre>';
-	};
+	}
 	if ($level == 0) {
 		$bagtrace = debug_backtrace();
 		$display  = ' style="dislay: none"';
 		if (isset($USER) && $USER->IsAdmin()) {
-			$display  = '';
+			$display = '';
 		}
 		echo '<div data-id="!!!----------DEBUG----------!!!" data-debug="' . $bagtrace[0]['file'] . ':' . $bagtrace[0]['line'] . '" .$display.>';
 		echo '<p style="font-size: 10px; color: #888">' . $bagtrace[0]['file'] . ':' . $bagtrace[0]['line'] . '</p>';
 		echo $out;
 		echo '</div>';
-	};
+	}
 	return $out;
-};
+}
 
-function print_r_php($value, $pre = false, $level = 0)
+/**
+ * print_r_php - вернет/выведит в браузер php код массива
+ * @param mixed $value - массив/данные
+ * @param mixed $echo - false/true выводить ли в браузер
+ * @param mixed $level - уровень вложенности (не нужно заполнять)
+ * @return string
+ */
+function print_r_php($value, $echo = false, $level = 0)
 {
 	global $USER;
 	$result = '';
-	$type = gettype($value);
+	$type   = gettype($value);
 	if ($type == 'array') {
 		$result .= '[' . "\n";
 		foreach ($value as $key => $val) {
@@ -82,19 +98,17 @@ function print_r_php($value, $pre = false, $level = 0)
 
 	if ($level == 0) {
 		$result .= ";";
-		if ($pre) {
+		if ($echo) {
 			$bagtrace = debug_backtrace();
 			if (isset($USER) && $USER->IsAdmin()) {
-				$result = '<pre data-id="!!!----------DEBUG----------!!!" data-debug="' . $bagtrace[0]['file'] . ':' . $bagtrace[0]['line'] . '">' .
-					'/* ' . $bagtrace[0]['file'] . ':' . $bagtrace[0]['line'] . '*/' . "\n" .
-					$result .
-					'</pre>';
+				$display = '';
 			} else {
-				$result = '<pre data-id="!!!----------DEBUG----------!!!" data-debug="' . $bagtrace[0]['file'] . ':' . $bagtrace[0]['line'] . '" style="dislay: none">' .
-					'/* ' . $bagtrace[0]['file'] . ':' . $bagtrace[0]['line'] . '*/' . "\n" .
-					$result .
-					'</pre>';
+				$display = ' style="dislay: none"';
 			}
+			$result = '<pre data-id="!!!----------DEBUG----------!!!" data-debug="' . $bagtrace[0]['file'] . ':' . $bagtrace[0]['line'] . '" ' . $display . '>' .
+				'/* ' . $bagtrace[0]['file'] . ':' . $bagtrace[0]['line'] . '*/' . "\n" .
+				$result .
+				'</pre>';
 			echo $result;
 		}
 	} else {
@@ -104,13 +118,20 @@ function print_r_php($value, $pre = false, $level = 0)
 	return $result;
 }
 
-function dtfp($arResult)
+/**
+ * dtfp - запишет в /__bx_debug.php скрипт показа массива.
+ * @param mixed $value - массив/данные
+ * @return void
+ */
+function dtfp($value)
 {
+	$bagtrace = debug_backtrace();
 	$filename = $_SERVER['DOCUMENT_ROOT'] . '/__bx_debug.php';
-	$text  = '<? require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");';
+
+	$text  = '<?php require_once("' . __FILE__ . '");';
 	$text .= 'echo "<hr>";';
-	$text .= 'echo "<p>' . date('Y.m.d H:i:s') . " " . __FILE__ . ':' . __LINE__ . '</p>";';
-	$text .= '$ar=' . print_r_php($arResult) . ';';
+	$text .= 'echo "<p>' . date('Y.m.d H:i:s') . ' ' . $bagtrace[0]['file'] . ':' . $bagtrace[0]['line'] . '</p>";';
+	$text .= '$ar=' . print_r_php($value) . ';';
 	$text .= 'print_r_tree($ar);';
 	$text .= '?>';
 	file_put_contents($filename, $text, FILE_APPEND);
