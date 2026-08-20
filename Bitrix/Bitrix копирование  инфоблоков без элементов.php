3ae58536cef975babd0d568818990d5d4858a0ee
Ivan Shabanov <Ivan.shabanov@gmail.com>
2026-08-20 11:02:33 +0700

    feat (Bitrix.копирование Инфоблоков): изменен заголовок берем только необходимое

--- Bitrix/Bitrix копирование  инфоблоков без элементов.php
+++ Bitrix/Bitrix копирование  инфоблоков без элементов.php
@@ -1,5 +1,19 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+<?if(!defined('NO_KEEP_STATISTIC')){
+	define('NO_KEEP_STATISTIC', true);
+}
+
+if(!defined('STOP_STATISTICS')){
+	define('STOP_STATISTICS', true);
+}
+
+if(!defined('NO_AGENT_STATISTIC')){
+	define('NO_AGENT_STATISTIC', true);
+}
+
+if(!defined('NO_AGENT_CHECK')){
+	define('NO_AGENT_CHECK', true);
+};
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 
 CModule::IncludeModule("iblock");
 if (intval($_REQUEST["IBLOCK_ID_FIELDS"]) > 0) {
